cf2f53196acb9f13409652cce3089d20411e8a7d
Weng Xuetian <wengxt@gmail.com>
2013-01-10 19:23:47 -0500

    use global env script directory

--- kcontrol/kcm_imchooser.cpp
+++ kcontrol/kcm_imchooser.cpp
@@ -43,24 +43,6 @@ void KCMIMChooser::save()
     KConfigGroup group(m_config, "IMLauncher");
     QString profile = m_model->data(m_model->index(m_ui->comboBox->currentIndex()), InternalNameRole).toString();
     group.writeEntry<QString>("DefaultIM", profile);
-
-    QDir kdedir(KGlobal::dirs()->localkdedir());
-    if (!kdedir.exists("env"))
-        kdedir.mkpath("env");
-
-    QFile envScript(KGlobal::dirs()->localkdedir() + "env/" + "imenv.sh");
-
-    if (profile != "none") {
-        if (!envScript.open(QIODevice::WriteOnly | QIODevice::Text))
-            return;
-        envScript.write("#!/bin/sh\n");
-        envScript.write("kdelibexecdir=$(kde4-config --install libexec)\n");
-        envScript.write("[ -n \"${kdelibexecdir}\" ] && test -r \"${kdelibexecdir}/setimenv.sh\" && source \"${kdelibexecdir}/setimenv.sh\"\n");
-        envScript.close();
-    }
-    else {
-        envScript.remove();
-    }
 }
 
 void KCMIMChooser::defaults()
